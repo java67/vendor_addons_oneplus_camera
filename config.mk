b0d6d3cce5235da3a735d4b06ab92b7bc168c3f4
jhonboy121 <alfredmathew05@gmil.com>
2020-09-27 21:37:38 +0530

    add daxservice module

--- config.mk
+++ config.mk
@@ -3,7 +3,9 @@ PRODUCT_PACKAGES += \
     OnePlusCameraService \
     oneplus-framework-res \
     OnePlusGallery \
-		DolbyAtmos
+		DolbyAtmos \
+		daxService
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml
+    $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
+		$(LOCAL_PATH)/system/etc/permissions/com.dolby.daxservice..xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.dolby.daxservice.xml
