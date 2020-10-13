3abb08550540010503fdf2785fe82c27eb3a5368
Rick M <spookcity138@gmail.com>
2020-10-13 18:14:40 +0200

    Remove Dolby
    At least for now and adapt some for my needs

--- config.mk
+++ config.mk
@@ -2,8 +2,7 @@ PRODUCT_PACKAGES += \
     OnePlusCamera \
     OnePlusCameraService \
     oneplus-framework-res \
-    OnePlusGallery \
-		DolbyAtmos
+    OnePlusGallery
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml
